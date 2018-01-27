a8a0027ef4822e1bfc7586d782d031b65b670177
LittleDev0617 <kyutaeyou@gmail.com>
2018-01-27 19:08:10 +0900

    LittleDev0617 - Second

--- NaverEnglishDictionary/Form1.Designer.cs
+++ NaverEnglishDictionary/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.EnWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SearchButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(483, 44);
+            this.webBrowser1.TabIndex = 5;
+            this.webBrowser1.Visible = false;
+            // 
+            // EnWord
+            // 
+            this.EnWord.Location = new System.Drawing.Point(12, 12);
+            this.EnWord.Name = "EnWord";
+            this.EnWord.Size = new System.Drawing.Size(165, 21);
+            this.EnWord.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(274, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 21);
+            this.label1.TabIndex = 3;
+            // 
+            // SearchButton
+            // 
+            this.SearchButton.Location = new System.Drawing.Point(183, 10);
+            this.SearchButton.Name = "SearchButton";
+            this.SearchButton.Size = new System.Drawing.Size(85, 23);
+            this.SearchButton.TabIndex = 4;
+            this.SearchButton.Text = "Search";
+            this.SearchButton.UseVisualStyleBackColor = true;
+            this.SearchButton.Click += new System.EventHandler(this.SearchButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 44);
+            this.Controls.Add(this.SearchButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.EnWord);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.TextBox EnWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button SearchButton;
     }
 }
 
--- NaverEnglishDictionary/Form1.cs
+++ NaverEnglishDictionary/Form1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace NaverEnglishDictionary
 {
@@ -15,6 +11,50 @@ namespace NaverEnglishDictionary
         public Form1()
         {
             InitializeComponent();
+            webBrowser1.ScriptErrorsSuppressed = true;
+        }
+
+        private void SearchButton_Click(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("http://endic.naver.com/search.nhn?sLn=kr&isOnlyViewEE=N&query=" + EnWord.Text);
+            while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            HtmlDocument doc = webBrowser1.Document;
+            HtmlElementCollection trs = doc.GetElementsByTagName("a");
+
+            foreach (HtmlElement el in trs)
+            {
+                string cn = el.GetAttribute("className");
+                if (cn.Contains("N=a:wrd.entry,r:1,"))
+                {
+                    string de = el.GetAttribute("href");
+                    webBrowser1.Navigate(de);
+                    while (webBrowser1.ReadyState != WebBrowserReadyState.Complete)
+                    {
+                        Application.DoEvents();
+                    }
+                    HtmlDocument doc2 = webBrowser1.Document;
+                    HtmlElementCollection c = doc2.GetElementsByTagName("span");
+                    foreach (HtmlElement d in c)
+                    {
+                        if (d.InnerText == "과거분사")
+                        {
+                            HtmlElement par = d.Parent;
+                            HtmlElementCollection child = par.Children;
+                            foreach(HtmlElement pp in child)
+                            {
+                                if(pp.GetAttribute("className") == "fnt_e07")
+                                {
+                                    label1.Text = pp.InnerText;
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
